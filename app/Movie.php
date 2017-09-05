4cb4856c8854a006eb8c7addac3bdfc4c434fcc7
OgnjenPetrovic <zoomght@gmail.com>
2017-09-05 20:14:28 +0200

    DRUGI ZADATAK VEZBE

--- app/Movie.php
+++ app/Movie.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     
+	protected $fillable = [
+        'name',
+        'director',
+        'imageUrl',
+        'duration',
+        'releaseDate',
+        'genres'
+    ];
+
     public function getGenresAttribute($value)
     {
         return json_decode($value);
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
-
 use Illuminate\Http\Request;
-
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,7 +13,12 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+ Route::get('movies','MoviesController@index');
+ Route::post('movies','MoviesController@store');
+ Route::get('movies/{id}','MoviesController@show');
+ Route::delete('movies/{id}','MoviesController@destroy');
+ Route::put('movies/{id}','MoviesController@update');
+//Route::resource('movies','MoviesController');
